7e17400cec17fce9747ca7b03e70903b14c663db
Bogdan.m.i <Bogdan.m.i@yandex.ru>
2021-11-29 22:34:08 +0500

    06/08

--- project/src/App.vue
+++ project/src/App.vue
@@ -3,25 +3,41 @@
   <div class="container">
     <div class="card">
       <h2>Актуальные новости {{ now }}</h2>
+      <span>Открыто: {{ openRate }}</span>
     </div>
-    <div class="card" v-for="item in news" :key="item">
-      <h3>{{ item }}</h3>
-      <button class="btn">Открыть</button>
-    </div>
+    <AppNews
+      v-for="item in news"
+      :key="item.id"
+      :title="item.title"
+      :id="item.id"
+      :is-open="item.isOpen"
+      @open-news="openNews"
+    />
   </div>
 </template>
 
 <script>
+import AppNews from './AppNews'
+
 export default {
   name: 'App',
   data: () => ({
     now: new Date().toLocaleDateString(),
+    openRate: 0,
     news: [
-      'News one',
-      'News two',
-      'News three',
-    ],
-  })
+      { id: 1, title: 'News one', isOpen: false },
+      { id: 2, title: 'News two', isOpen: false },
+      { id: 3, title: 'News three', isOpen: false }
+    ]
+  }),
+  methods: {
+    openNews () {
+      this.openRate++
+    }
+  },
+  components: {
+    AppNews
+  }
 }
 </script>
 
---
+++ project/src/AppNews.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="card">
+    <h3>{{ title }}</h3>
+    <button class="btn" @click="open">{{ isNewsOpen ? 'Закрыть' : 'Открыть' }}</button>
+    <p v-if="isNewsOpen">Lorem ipsum dolor sit amet.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  // props: ['title'],
+  // emits: ['open-news'],
+  name: 'AppNews',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    id: {
+      type: Number,
+      required: true
+    },
+    isOpen: {
+      type: Boolean,
+      required: false,
+      default: false,
+      validator (val) {
+        return true
+      }
+    }
+  },
+  emits: {
+    'open-news' (num) {
+      if (num) return true
+      console.warn('no data in open-news emit')
+      return false
+    }
+  },
+  data () {
+    return {
+      isNewsOpen: this.isOpen
+    }
+  },
+  methods: {
+    open () {
+      this.isNewsOpen = !this.isNewsOpen
+      if (this.isNewsOpen) this.$emit('open-news', 42)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
