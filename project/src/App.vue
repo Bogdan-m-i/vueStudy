0dc11692b5668bf1c94fd0ef060d275be17ae237
Bogdan-m-i <bogdan.m.i@yandex.ru>
2021-11-29 19:43:41 +0500

    new commit

---
+++ project/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <the-header/>
+  <div class="container">
+    <div class="card">
+      <h2>Актуальные новости {{ now }}</h2>
+    </div>
+    <div class="card" v-for="item in news" :key="item">
+      <h3>{{ item }}</h3>
+      <button class="btn">Открыть</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    now: new Date().toLocaleDateString(),
+    news: [
+      'News one',
+      'News two',
+      'News three',
+    ],
+  })
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ project/src/TheHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <h1>The Header</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TheHeader",
+    data: () => ({
+      title: 'The header',
+    })
+  }
+</script>
+
+<style scoped>
+
+</style>
